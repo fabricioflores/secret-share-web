9df52f91dc6358f6c7649239c8f5fb811fed3bff
Fabricio Flores <fabrifloresg@gmail.com>
2017-08-02 15:28:38 -0500

    remove database references

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170725195645) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "secrets", force: :cascade do |t|
-    t.string   "description"
-    t.integer  "available"
-    t.integer  "needed"
-    t.text     "splits",      default: [],              array: true
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
