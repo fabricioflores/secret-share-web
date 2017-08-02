54c297a45b88f2b4efb5c56d147bf30382bb98eb
Fabricio Flores <fabrifloresg@gmail.com>
2017-08-02 15:40:23 -0500

    update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,7 @@ set :repo_url, "git@github.com:fabricioflores/secret-share-web.git"
 set :branch, :master
 set :deploy_to, '/home/deploy/secret-share-web'
 set :pty, true
+set :linked_files, %w{config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
